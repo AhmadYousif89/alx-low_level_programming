5f0d823cb1cf780199112cd7e60cdd37b10856e6
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2023-10-08 02:34:14 +0300

    Print the lowest three digit combination using putchar()

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Print the lowest three digit combination using putchar();
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int i, j, k;
+
+    for (i = '0'; i <= '9'; i++)
+    {
+        for (j = i + 1; j <= '9'; j++)
+        {
+            for (k = j + 1; k <= '9'; k++)
+            {
+                putchar(i);
+                putchar(j);
+                putchar(k);
+                if (i < '7')
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+
+    return (0);
+}

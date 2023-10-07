148846c21260b03992825ada6d8d46c410a48a3f
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2023-10-08 02:39:47 +0300

    Print the lowest three digit combination using putchar()

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,26 +13,26 @@
 
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    for (i = '0'; i <= '9'; i++)
-    {
-        for (j = i + 1; j <= '9'; j++)
-        {
-            for (k = j + 1; k <= '9'; k++)
-            {
-                putchar(i);
-                putchar(j);
-                putchar(k);
-                if (i < '7')
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }

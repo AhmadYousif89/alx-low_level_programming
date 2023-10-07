a2170cc256532004142c05fc84cdc66595298140
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2023-10-07 22:02:24 +0300

    Print all possible combination of single number using putchar()

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Print all possible combination of single number using putchar();
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+    int j;
+
+	for (i = '0'; i <= '9'; i++)
+    {
+        putchar(i);
+        if (i < '9')
+            putchar(',');
+		putchar(' ');
+    }
+	putchar('\n');
+	return (0);
+}

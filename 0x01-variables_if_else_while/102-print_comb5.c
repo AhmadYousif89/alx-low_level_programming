fd2ac6f4eb8bb0583cbe2fa0273ff1a590219fc1
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2023-10-08 04:06:10 +0300

    Print all possible combinations of two two-digit numbers using putchar()

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Print all possible combinations of two two-digit numbers using putchar();
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first_digits, second_digits;
+
+	for (first_digits = 0; first_digits <= 99; first_digits++)
+	{
+		for (second_digits = first_digits; second_digits <= 99; second_digits++)
+		{
+			/* Prevent dublicate numbers */
+			if (second_digits != first_digits)
+			{
+				/* Print the first digit of the first two-digits ==> 0 .. 9 */
+				putchar(first_digits / 10 + 48);
+				/* Print the second digit of the first two-digits ==> 01 will print 1 */
+				putchar(first_digits % 10 + 48);
+				putchar(' ');
+
+				// Print the second two-digit number
+				putchar(second_digits / 10 + 48);
+				putchar(second_digits % 10 + 48);
+
+				// Add a comma and space if not the last combination ==> [.., 97 99, 98 99]
+				if (first_digits != 98 || second_digits != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return 0;
+}

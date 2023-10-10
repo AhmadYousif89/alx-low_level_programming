a10b955c44338f94e2e025251733178f55c885fd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-10 20:56:38 +0300

    Print the alphabet 10x in lowercase using for loop and putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print string 10x to stdio
+ * Print the alphabet 10x in lowercase using for loop and putchar();
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}

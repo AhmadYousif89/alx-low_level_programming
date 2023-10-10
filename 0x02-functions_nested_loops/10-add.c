d29f508f04efdf7e50f8b0e75cd371c4e27d5791
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-11 01:29:41 +0300

    Add two integers and return the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Addition of two integers.
+ * Description: Add two integers and return the sum.
+ * @num1: data of type integer
+ * @num2: data of type integer
+ * Return: (sum)
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * times_table - Print 24 hour (00:00 .. 23:59) format.
+ * Description:
+ * Print out the characters (00:00) to (23:59).
+ * Return: Void
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	}
+}

aec509c3b1e6073914c51dfb4f64497828fa98d6
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-10 21:34:16 +0300

    Check if character is lowercase or uppercase alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check if character is lowercase or uppercase alphabet
+ * Description - Check and return (1) if lower or upper alphabet, (0) otherwise
+ * @c: arg of type int
+ * Return: 1 (c is alphabet) or 0 (otherwise)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}

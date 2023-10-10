140b437ed664ac2b247dc13a65f63b217310e1c4
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-10 15:52:35 +0300

    create 0x02 folder and readme file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print string "_putchar" using printf();
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_puchar\n");
+	return (0);
+}

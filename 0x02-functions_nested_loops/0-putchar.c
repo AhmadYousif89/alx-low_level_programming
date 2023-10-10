76bf6f3ce7d9a2657b0cfbd3642d4b0ce5095ea6
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-10 18:08:03 +0300

    Print string "_putchar" using while loop and putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
 /**
  * main - Entry point
- * Print string "_putchar" using printf();
+ * Print string "_putchar" using while loop and putchar();
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_puchar\n");
+	char str[] = "_puchar";
+	int str_len = strlen(str);
+	int i = 0;
+
+	while (i < str_len)
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+
 	return (0);
 }

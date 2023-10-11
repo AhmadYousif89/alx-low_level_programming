f16e8fa6c5ec9bd0d7a23db8dabe75ec13380fe7
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-11 03:27:25 +0300

    Print out a grid based on a given number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - Print a grid of (n) by (n).
+ * Description: Print out a grid based on a given number
+ * representing the times table of that number
+ * @n: data of type int
+ * Return: Void
+ */
+
+void print_times_table(int n)
+{
+	int x, y, multi;
+
+	if (n > 12)
+		return;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			multi = x * y;
+			if (y == 0)
+				printf("%d", multi);
+			else
+				printf("%3d", multi);
+			if (y < n)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}

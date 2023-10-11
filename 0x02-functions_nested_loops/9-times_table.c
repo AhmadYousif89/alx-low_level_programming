8ecec5814fb7834608180d0d9b675e76510e720e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-11 03:00:59 +0300

    Print a grid of 9 by 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,16 +1,25 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * times_table - Print 24 hour (00:00 .. 23:59) format.
- * Description:
- * Print out the characters (00:00) to (23:59).
+ * times_table - Print a grid of 9 by 9.
+ * Description: Print out a grid 9 * 9 representing the times table of number 9
  * Return: Void
  */
+
 void times_table(void)
 {
-	int i;
+	int x, y;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x <= 9; x++)
 	{
+		for (y = 0; y <= 9; y++)
+		{
+			printf("%2d", x * y);
+			if (y < 9)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
 	}
 }

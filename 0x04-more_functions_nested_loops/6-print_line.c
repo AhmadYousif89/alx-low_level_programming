3e73ab567cf7b2450f6c411595be48e854250e2b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-12 16:04:21 +0300

    Print straight lines in the terminal based on given number

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Draw lines in the terminal
+ * Description - Print straight lines in the terminal based on given number
+ * @n: number of lines to be print
+ * Return: Void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

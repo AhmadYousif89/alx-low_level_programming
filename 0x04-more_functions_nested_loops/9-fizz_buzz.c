11228160e7c1681325064326488c046ff0687df0
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-12 18:05:22 +0300

    Print FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,26 +11,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 15 == 0)
 		{
-			printf(" Fizz");
+			printf("FizzBuzz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 3 == 0)
 		{
-			printf(" Buzz");
+			printf("Fizz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
+			if (i != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
 		}
 		else
-		{
-			printf(" %d", i);
-		}
+			printf("%d ", i);
 	}
 	printf("\n");
 

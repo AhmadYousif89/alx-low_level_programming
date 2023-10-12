c2145d576a1f67f84c5061c601b6c87caf8c3e43
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-13 02:00:27 +0300

    Calculate the largest prime factor for a given number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,26 +8,23 @@
 
 int main(void)
 {
-	long num = 11456233;
-	long largest_prime_factor;
-	long smallest_prime_factor = 2;
+	unsigned long num = 612852475143;
+	unsigned long largest_prime_factor = 2;
 
+	printf("num: %lu\n", num);
 	/* Loop over (num) until it reach (1) */
 	while (num > 1)
 	{
-		/* Increment the smallest possible prime factor */
-		/* if this factor doesn't divide the given (num) evenly */
-		while (num % smallest_prime_factor != 0)
+		if (num % largest_prime_factor == 0)
 		{
-			smallest_prime_factor++;
+			num /= largest_prime_factor;
 		}
-		/* Divide (num) on the new smallest prime factor */
-		num /= smallest_prime_factor;
-		/* The largest prime factor is the last possible */
-		/* dividable number for (num) */
-		largest_prime_factor = smallest_prime_factor;
+		else
+			/* Increment the largest possible prime factor */
+			largest_prime_factor++;
 	}
-	printf("%ld\n", largest_prime_factor);
+
+	printf("largest_prime_factor: %lu\n", largest_prime_factor);
 
 	return (0);
 }

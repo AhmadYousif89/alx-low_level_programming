34ec3d4dbbe41dd733f80e3e4f7f2409ddd22556
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-13 02:30:11 +0300

    Calculate the largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,32 +9,24 @@
 
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+	/*int i;*/
+	long num = 612852475143;
+	long largest_prime_factor = 2;
+	/*int squar_num = sqrt(num);*/
 
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
+	/* Loop over (num) until it reach (1) */
+	while (num > 1)
 	{
-		max = 2;
-		n /= 2;
-	}
-
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
+		if (num % largest_prime_factor == 0)
 		{
-			max = i;
-			n = n / i;
+			num /= largest_prime_factor;
 		}
+		else
+			/* Increment the largest possible prime factor */
+			largest_prime_factor++;
 	}
 
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
+	printf("%ld\n", largest_prime_factor);
 
 	return (0);
 }

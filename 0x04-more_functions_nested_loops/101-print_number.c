404241d311638e9da4ce85c1a98b831865ac83f4
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-13 03:36:21 +0300

    Print integer numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,38 +6,24 @@
  * @n: number
  * Return: Void
  */
-
 void print_number(int n)
 {
-	unsigned int num = n;
-	unsigned int reversed_n = 0;
+	unsigned int n1;
 
-	/* Case: n = 0 */
-	if (num == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	/* Case: n is negative => -98 */
 	if (n < 0)
 	{
-		/* Print (-) and negate the number (num) */
+		n1 = -n;
 		_putchar('-');
-		num = -n;
 	}
-	/* n = 98 */
-	/* Loop over (num) and reverse it => 98 to 89 in 2 iterations */
-	while (num > 0)
+	else
 	{
-		/* On first iteration (reversed_n) => 8 */
-		/* On second iteration (reversed_n) => 9 */
-		reversed_n = reversed_n * 10 + num % 10;
-		num /= 10;
+		n1 = n;
 	}
-	/* Print the reversed characters => 98 */
-	while (reversed_n > 0)
+
+	if (n1 / 10)
 	{
-		_putchar('0' + reversed_n % 10);
-		reversed_n /= 10;
+		print_number(n1 / 10);
 	}
+
+	_putchar((n1 % 10) + '0');
 }

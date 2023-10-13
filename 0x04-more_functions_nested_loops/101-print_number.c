90768bf08f7ce08f65912084dc7b280a94c0909b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-13 03:46:47 +0300

    Print any given number using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,28 +2,27 @@
 
 /**
  * print_number - Print integer numbers
- * Description - Print any given number using only _putchar
+ * Description - Print any given number using _putchar
  * @n: number
  * Return: Void
  */
 void print_number(int n)
 {
-	int n1;
+	unsigned int num;
 
+	/* Case: n is negative -- print (-) and negate (n) */
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
+		num = -n;
 	}
 	else
+		num = n;
+	/* Recursively invoking (print_number) to print the numbers in reverse */
+	if (num / 10)
 	{
-		n1 = n;
+		print_number(num / 10);
 	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	/* Print the last number of (num) */
+	_putchar((num % 10) + '0');
 }

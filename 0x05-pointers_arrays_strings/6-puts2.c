6d0031c825b40f0dec3fb54c092534f7b488ed6f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-17 00:13:44 +0300

    Print every other number

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts2 - Print every other number
@@ -8,13 +9,21 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, j, len;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
-			_putchar(str[i]);
 		i++;
+	}
+
+	len = i - 1;
+
+	for (j = 0; j <= len; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
 	}
 	_putchar('\n');
 }

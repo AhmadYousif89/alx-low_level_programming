35ef99dcf611fbff4dd732e7c3a1da26b75238d0
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-17 23:23:42 +0300

    Generate random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,28 +9,29 @@
 
 int main(void)
 {
-	int j, random;
-	int i = 0, ascii = 2772;
+	int i = 0;
+	int c, random;
+	int ascii = 2772;
 	char password[50]; /* Password length */
 
 	srand(time(NULL));
-	while (ascii > 126)
+	while (ascii > 127)
 	{
-		random = rand() % 126;
+		random = rand() % 127;
 		password[i] = random;
 		ascii -= random;
 		i++;
 	}
+
 	if (ascii > 0)
 		password[i] = ascii;
 	else
-	{
 		i--;
+
+	for (c = 0; c <= i; c++)
+	{
+		printf("%c", password[c]);
 	}
 
-	for (j = 0; j <= i; j++)
-	{
-		printf("%c", password[j]);
-	}
 	return (0);
 }

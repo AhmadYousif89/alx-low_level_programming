6bdf680a66bc5d3d431b5ee7e19efc6bf4da4d07
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-19 21:44:39 +0300

    magic number

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * infinite_add - Add return two numbers
+ * @n1: pointer type char *
+ * @n2: pointer type char *
+ * @r: pointer type char *
+ * @size_r: number type int
+ * Return: void
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}

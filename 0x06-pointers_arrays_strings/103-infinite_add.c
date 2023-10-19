5f0dcab35f13ec99428eca1974292ecc1a7fc70b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-20 01:51:02 +0300

    Add and return two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,44 +12,38 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, n1_len = strlen(n1), n2_len = strlen(n2);
-	int big_len = 0, cur_n1_digit = 0, cur_n2_digit = 0, acc = 0;
 	char *buffer = r;
+	int i, n1_digit, n2_digit, sum, carry;
+	int n1_len = strlen(n1), n2_len = strlen(n2);
+	int big_len = (n1_len > n2_len) ? n1_len : n2_len;
 
-	if (n1_len > n2_len)
-		big_len = n1_len;
-	else
-		big_len = n2_len;
 	if (size_r < big_len + 1)
 		return (0);
+
 	buffer[big_len] = '\0';
+
 	for (i = big_len - 1; i >= 0; i--)
 	{
 		n1_len--;
 		n2_len--;
-		if (n1_len >= 0)
-			cur_n1_digit = n1[n1_len] - '0';
-		else
-			cur_n1_digit = 0;
-		if (n2_len >= 0)
-			cur_n2_digit = n2[n2_len] - '0';
-		else
-			cur_n2_digit = 0;
-		buffer[i] = (cur_n1_digit + cur_n2_digit + acc) % 10 + '0';
-		acc = (cur_n1_digit + cur_n2_digit + acc) / 10;
+		/* Extract the digits and add to the buffer*/
+		n1_digit = (n1_len >= 0) ? n1[n1_len] - '0' : 0;
+		n2_digit = (n2_len >= 0) ? n2[n2_len] - '0' : 0;
+		sum = n1_digit + n2_digit + carry;
+		carry = sum / 10;
+		buffer[i] = sum % 10 + '0';
 	}
-	if (acc == 1)
-	{
-		buffer[big_len + 1] = '\0';
-		if (size_r < big_len + 2)
-			return (0);
-		while (big_len >= 0)
-		{
-			buffer[big_len + 1] = buffer[big_len];
-			big_len--;
-		}
-		buffer[0] = acc + '0';
-	}
+
+	/* Case we have a carry over, store it in the most significant digit */
+	buffer[big_len] = '\0';
+	if (size_r < big_len + 2)
+		return (0);
+
+	while (big_len-- >= 0)
+		buffer[big_len + 1] = buffer[big_len];
+
+	if (carry)
+		buffer[0] = carry + '0';
 
 	return (buffer);
 }

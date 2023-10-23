86c8f591262d61a059298d35c1553d2e872ffde9
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-23 22:46:20 +0300

    Search a string for any set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,8 +4,8 @@
  * _strpbrk - Search a string for any set of bytes
  * @s: pointer type char
  * @accept: pointer type char
- * Return: the number of bytes in the initial segment of s
- * which consist only of bytes from accept
+ * Return: Pointer to the byte in (s) that matches one of the bytes in accept
+ * or NULL
  */
 char *_strpbrk(char *s, char *accept)
 {

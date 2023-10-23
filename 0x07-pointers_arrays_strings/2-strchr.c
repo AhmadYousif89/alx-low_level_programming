3f4df10dabba6382cb67342cdb6e40e291d0bf94
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-23 21:57:08 +0300

    Locate character and print string from its position

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * _strchr - Locate character and print string from its position
@@ -12,8 +11,14 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
 		s++;
 	}
-	return ('\0');
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
 }

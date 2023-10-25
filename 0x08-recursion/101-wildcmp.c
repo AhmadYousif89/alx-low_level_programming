f6b5d193a8c1ccc5c66938efcfd10d1163fb8f64
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-25 23:56:24 +0300

    Compares two strings and return a number

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * wildcmp - Compares two strings and return a number | recursion
  * @s1: pointer type char
@@ -9,6 +10,7 @@ int wildcmp(char *s1, char *s2)
 	/* Case 1: both strings are empty, they are considered identical */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+
 	/* Case 2: s2 has a wildcard '*' */
 	if (*s2 == '*')
 	{
@@ -20,9 +22,11 @@ int wildcmp(char *s1, char *s2)
 		if (*s2 == '\0')
 			return (1);
 	}
-	/* Case 4: s2 has no '*' and didn't match any character in s1 */
+
+	/* Case 3: s2 doesn't have '*' and doesn't match any character in s1 */
 	if (*s1 != *s2)
 		return (0);
+
 	/* Move to the next character position in both strings */
 	return (wildcmp(s1 + 1, s2 + 1));
 }

231dc219a60df73ef3421551e458d4437d3da7ac
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-25 23:34:38 +0300

    Compares two strings and return a number | recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,21 +12,13 @@ int wildcmp(char *s1, char *s2)
 	/* Case 2: s2 has a wildcard '*' */
 	if (*s2 == '*')
 	{
-		/* Move s2 until it reaches the next character after '*' */
-		s2++;
-		/*  We try to match evry character in s2 with s1*/
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2))
-				return (1);
-			s1++;
-		}
+		/*  Try to match evry character in s2 with s1 after '*' in s2 */
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+			return 1;
 		/* If '*' is the last char in s2 */
 		/* that means that s2 can match any char in s1 so we return (1) */
 		if (*s2 == '\0')
 			return (1);
-		/* Case 3: we didn't find any matching characters with s1 */
-		return (0);
 	}
 	/* Case 4: s2 has no '*' and didn't match any character in s1 */
 	if (*s1 != *s2)

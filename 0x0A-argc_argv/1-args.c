2b13d30964113335059566aeab76a8a70563ed92
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-27 17:04:23 +0200

    Print the count of args excluding the file name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,21 +6,8 @@
  * @argv: array of char*
  * Return: (0)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	if (argc <= 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		while (*argv)
-		{
-			i++;
-			argv++;
-		}
-		printf("%d\n", i - 1);
-	}
-
+	printf("%d\n", argc - 1);
 	return (0);
 }

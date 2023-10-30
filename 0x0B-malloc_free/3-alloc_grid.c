af68e19456ca28a6ae7c68e581fb13ac066e5b83
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-10-30 22:00:15 +0200

    Allocate memory for 2 dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	/* Allocate memory for the rows */
-	arr = (int **)malloc(sizeof(int) * height);
+	arr = (int **)malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
 		return (NULL);

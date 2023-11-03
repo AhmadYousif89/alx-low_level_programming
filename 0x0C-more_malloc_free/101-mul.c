4ec750f2ea906670b3482fec3cf2afb2570f0469
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-03 04:27:54 +0200

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,16 +20,13 @@ unsigned long int convertToInt(char *s)
 	int sign = 1;
 	unsigned long int i, j, res = 0;
 
-	for (i = 0; !(isDigit(s[i])); i++)
+	for (i = 0; !(isdigit(s[i])); i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
 	}
-	for (j = i; isDigit(s[j]); j++)
-	{
-		res *= 10;
-		res += s[i] - '0';
-	}
+	for (j = i; isdigit(s[j]); j++)
+		res = res * 10 + s[i] - '0';
 
 	return (res * sign);
 }
@@ -42,36 +39,12 @@ unsigned long int convertToInt(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	unsigned long int mul;
-	char *arg1;
-	char *arg2;
 
 	if (argc != 3)
 	{
 		_print("Error");
 		exit(98);
-	}
-
-	arg1 = argv[1];
-	arg2 = argv[2];
-
-	for (i = 0; arg1[i] != '\0'; i++)
-	{
-		if (!isdigit(arg1[i]))
-		{
-			_print("Error");
-			exit(98);
-		}
-	}
-
-	for (i = 0; arg2[i] != '\0'; i++)
-	{
-		if (!isdigit(arg2[i]))
-		{
-			_print("Error");
-			exit(98);
-		}
 	}
 
 	mul = convertToInt(argv[1]) * convertToInt(argv[2]);

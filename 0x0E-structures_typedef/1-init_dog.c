70a7edb3dd8111821c4a7e66d758f932b0a18e7a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-06 21:42:46 +0200

    Create a dog structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * init_dog - Create a dog structure
+ * @d: pointer to the dog struct
+ * @name: the dog name -> string
+ * @age: the dog age -> float
+ * @owner: the owner of the dog -> string
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
+}

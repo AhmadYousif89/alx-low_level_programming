dc14c29ddf36c27d19100fab98bd42a9f73d13b2
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-08 13:20:18 +0200

    Print a name using pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - Print a name using pointer to function
+ * @name: name of the person
+ * @f: pointer to function
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}

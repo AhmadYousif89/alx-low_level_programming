e6b0d1906e1343f21dc48d699c32097b34d300bd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-09 18:36:42 +0200

    Return the sum of the var-args

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,21 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - Return the sum of all args
+ * sum_them_all - Return the sum of the var-args
  * @n: constant positive number
  * Return: the sum
  */
-int sum_them_all(const unsigned int n, ...) {}
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list(li);
+	va_start(li, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(li, int);
+
+	return (sum);
+}

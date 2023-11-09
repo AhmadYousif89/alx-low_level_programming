33b89008873ac41e0372ccf5fbeec383901f6dd1
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-09 20:29:26 +0200

    Print any argument matching "cifs"

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,44 @@
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * print_all - Print all args
+ * print_all - .
  * @format: pointer to string
  * Return: Nothing
  */
-void print_all(const char *const format, ...) {}
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str;
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c ", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d ", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f ", va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s ", str);
+			break;
+		default:
+			break;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}

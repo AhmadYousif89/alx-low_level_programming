68533c00e4039c025298f71ba07c665edad744a4
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-09 19:31:21 +0200

    Print numbers with comma separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,9 +1,26 @@
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_numbers - Print numbers
+ * print_numbers - Print numbers with comma separator
  * @separator: pointer to string
  * @n: const positive number
  * Return: Nothing
  */
-void print_numbers(const char *separator, const unsigned int n, ...) {}
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list(li);
+	va_start(li, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(li, int);
+
+		if (i == n - 1)
+			printf("%i\n", num);
+		else
+			printf("%i%s", num, separator);
+	}
+}

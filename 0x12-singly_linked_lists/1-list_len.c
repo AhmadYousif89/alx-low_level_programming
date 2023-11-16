3a2fa10226494fc5217d7333626cfa0ee30775b3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-16 20:08:51 +0200

    Get the length of a list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * list_len - Print the number of elements in a list
+ * list_len - Get the length of a list
  * @h: pointer to the list struct
- * Return: number of nodes in the list
+ * Return: length of the list
  */
 size_t list_len(const list_t *h)
 {
@@ -12,9 +12,8 @@ size_t list_len(const list_t *h)
 	if (!h)
 		return (i);
 
-	while (h != NULL)
+	while (h)
 	{
-		!h->str ? printf("[0] (nil)\n") : printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}

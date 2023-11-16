ca119911a196fa96d1b9718c33f751bad02d48ef
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-16 22:32:02 +0200

    first - Run before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * first - Run before main
+ * Return: Nothing
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

07ee3c80381af9a7c759b4d3dd1bf3115334cf6f
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-16 20:01:13 +0200

    Print the number of nodes in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - Print the number of nodes in a list
+ * @h: pointer to the list struct
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	if (!h)
+		return (i);
+
+	while (h != NULL)
+	{
+		!h->str ? printf("[0] (nil)\n") : printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}

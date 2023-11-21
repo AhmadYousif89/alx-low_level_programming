53bab8a3b95cabeeeb18c2d52438cc9997df4fa3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-21 23:15:55 +0200

    Reverse nodes inside listint

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverse nodes inside listint
+ * @head: pointer to the pointer of listint
+ * Return: pointer to the head or NULL
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *cur = *head, *prev = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+
+	while (cur != NULL)
+	{
+		cur = (*head)->next;  /* Move to the next node */
+		(*head)->next = prev; /* Reverse the link */
+		prev = *head;		  /* Move to the next pair of nodes */
+		*head = cur;		  /* Move to the next pair of nodes */
+	}
+
+	*head = prev;
+
+	return (cur);
+}

ad19464f97a131dcda20765ea4d2800cce224a43
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-22 00:51:09 +0200

    Print-safe the nodes in listint

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,35 +7,31 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nc = 0;
-	const listint_t *current = head, *slow = head, *fast = head;
+	size_t i, nc = 0;
+	const listint_t *current = head, *next = NULL;
 
-	while (fast && fast->next)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		nc++;
-
-		fast = fast->next->next;
-		slow = slow->next;
-
-		if (fast == slow)
-		{
-			printf("-> [%p] %d\n", (void *)fast, fast->n);
-			return (nc);
-		}
-
-		current = current->next;
-	}
+	if (!head)
+		exit(98);
 
 	while (current)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
 		nc++;
-	}
 
-	if (!head)
-		exit(98);
+		current = current->next;
+		next = head;
+		i = 0;
+		while (i < nc)
+		{
+			if (current == next)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (nc);
+			}
+			next = next->next;
+			i++;
+		}
+	}
 
 	return (nc);
 }

e3d91c4ee7f8e1c735ed155928a73d01ac57aacd
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-22 18:34:01 +0200

    Free a linked list | safe version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -19,9 +19,9 @@ size_t free_listint_safe(listint_t **h)
 		/* Checks the difference in addrs between the cur node and the next */
 		dif = *h - (*h)->next;
 
-		/* If dif = 0 or is positive, then we are not in a loop */
+		/* If dif is positive, then we are not in a loop */
 		/* and the function frees the current node and moves on to the next */
-		if (dif >= 0)
+		if (dif > 0)
 		{
 			temp = (*h)->next;
 			free(*h);

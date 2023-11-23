ddc82d19428da0e34bc1a08425faafe7940412e7
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-23 20:19:51 +0200

    Set the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,8 +2,24 @@
 
 /**
  * set_bit - Set the value of a bit to 1 at a given index
- * @n: pointer to the number as string
+ * @n: pointer to the number
  * @index: the index of the bit
  * Return: 1 on success or -1 on failuer
  */
-int set_bit(unsigned long int *n, unsigned int index) {}
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bit = sizeof(*n) * 8 - 1;
+	/* Index out of range = failuer */
+	if (index > bit)
+		return (-1);
+	/* Check if the result of right shifting the value of (n)*/
+	/* by the index and bitwising with (& 1) will equal (0) */
+	if (((*n >> index) & 1) == 0)
+	{
+		/* Set the bit value to (1) */
+		*n = *n | (1 << index);
+		return (1);
+	}
+
+	return (-1);
+}
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * clear_bit - Set the value of a bit to 0 at a given index
- * @n: pointer to the number as string
+ * @n: pointer to the number
  * @index: the index of the bit
  * Return: 1 on success or -1 on failuer
  */

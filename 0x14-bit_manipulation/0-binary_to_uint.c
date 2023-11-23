54638ef1a2c29707ce355c343945beed9ac37c8a
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-23 16:32:44 +0200

    Convert binary to positive number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,4 +6,22 @@
  * Return: -
  * The converted number or (0) if *b is NULL or contain char other than 1 or 0
  */
-unsigned int binary_to_uint(const char *b) {}
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int n = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		n = n * 2 + (b[i] - '0');
+		i++;
+	}
+
+	return (n);
+}

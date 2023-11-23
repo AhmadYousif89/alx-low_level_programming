51110af2397796b9ab4f1933732dafee5384e01b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-23 20:52:42 +0200

    Flip the bits of a given number to get to another number

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,4 +6,16 @@
  * @m: the modified number
  * Return: 1 on success or -1 on failuer
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m) {}
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int xor_result = n ^ m;
+
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+
+	return (count);
+}

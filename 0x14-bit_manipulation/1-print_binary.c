281d1a45c59447b4afc13f427b71745ed0e5b5d3
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-23 19:08:46 +0200

    Print numbers in binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,4 +5,25 @@
  * @n: the number to print
  * Return: Nothing
  */
-void print_binary(unsigned long int n) {}
+void print_binary(unsigned long int n)
+{
+	/* The most significant bit (MSB) position, e.g., (63) for a 64-bit */
+	int bit = sizeof(n) * 8 - 1;
+	int printed = 0;
+
+	while (bit >= 0)
+	{
+		/* Get the value of the current bit by right shifting (n) */
+		/* by 'bit' positions and bitwise AND with 1 */
+		if ((n >> bit) & 1)
+		{
+			putchar('1');
+			printed = 1;
+		}
+		/* If 'bit' is equal to 0 or it was printed, then print '0' */
+		else if (printed || bit == 0)
+			putchar('0');
+		/* Decrement 'bit' to move toward the MSB */
+		bit--;
+	}
+}

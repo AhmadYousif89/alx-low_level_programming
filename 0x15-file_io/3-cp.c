65b4d753a821f0d9af4a53c3aaa91cc1a5f18640
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-27 23:35:30 +0200

    Copy the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,18 @@
 #include "main.h"
+#define USAGE "Usage: cp file_from file_to\n"
+#define ErrMsgOnR "Error: Can't read from file %s\n"
+#define ErrMsgOnW "Error: Can't write to file %s\n"
+#define ErrMsgOnC "Error: Can't close fd %i\n"
 
 /**
- * exit_werr - Helper function
+ * handle_err - Helper function to handle errors
  * @code: the exit number
  * @message: the error message to print upon exit
  * @arg: pointer to the optional arguments
  */
-void exit_werr(int code, const char *message, const char *arg)
+void handle_err(int code, const char *message, const char *arg)
 {
-	dprintf(2, message, arg);
+	dprintf(STDERR_FILENO, message, arg);
 	exit(code);
 }
 
@@ -20,44 +24,41 @@ void exit_werr(int code, const char *message, const char *arg)
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to;
+	int fd_from = 0, fd_to = 0;
 	char buffer[BUFF_SIZE];
 	const char *file_from, *file_to;
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
-		exit_werr(97, "Usage: cp file_from file_to\n", NULL);
+		handle_err(97, USAGE, NULL);
 
 	file_from = argv[1];
 	file_to = argv[2];
+
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-		exit_werr(98, "Error: Can't read from file %s\n", file_from);
+		handle_err(98, ErrMsgOnR, file_from);
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		close(fd_from);
-		exit_werr(99, "Error: Can't write to file %s\n", file_to);
-	}
+		handle_err(99, ErrMsgOnW, file_to);
+
 	while ((bytes_read = read(fd_from, buffer, BUFF_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			exit_werr(99, "Error: Can't write to file %s\n", file_to);
-		}
+			handle_err(99, ErrMsgOnW, file_to);
 	}
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		exit_werr(98, "Error: Can't read from file %s\n", file_from);
-	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		exit_werr(100, "Error: Can't close file descriptor\n", NULL);
 
-	return (0);
+	if (bytes_read == -1)
+		handle_err(98, ErrMsgOnR, file_from);
+
+	fd_from = close(fd_from);
+	fd_to = close(fd_to);
+	if (fd_from)
+		handle_err(100, ErrMsgOnC, fd_from);
+	if (fd_to)
+		handle_err(100, ErrMsgOnC, fd_to);
+
+	return (EXIT_SUCCESS);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define BUFF_SIZE 1024
+const size_t BUFF_SIZE = 1024;
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

d797281a863008dcd2ada621e71bafa8d0b5a371
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-27 16:04:02 +0200

    Read a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - Read a text file and prints it to the POSIX standard output
+ * @filename: pointer to the filename
+ * @letters: count of letters to read
+ * Return: The count of letter or (0) on failuer
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nb = 0;
+	char buffer[BUFF_SIZE];
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	nb = read(fd, &buffer, letters);
+	nb = write(STDOUT_FILENO, &buffer, nb);
+	close(fd);
+
+	return (nb);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
+
+#define BUFF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

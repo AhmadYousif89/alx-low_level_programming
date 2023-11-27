4925e1764a3c7c3ee2f8850dfd01f14f77bca6d8
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-27 16:04:36 +0200

    Read a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nb = 0;
-	char buffer[BUFF_SIZE];
+	char buffer[BUFF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);

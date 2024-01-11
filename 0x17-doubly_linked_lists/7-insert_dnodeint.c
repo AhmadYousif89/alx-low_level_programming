c10a8e5be272b8bf500094b205fd493941234dee
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-11 21:06:00 +0200

    Insert new node at a given index of the dlistint.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,9 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = NULL;
 	new->prev = NULL;
 	/* Insert at the beginning of the list if idx = 0 */
-	if (!*h)
-		return (NULL);
-	if (idx == 0)
+	if (idx == 0 || !*h)
 	{
 		new->next = *h;
 		(*h)->prev = new;
@@ -31,18 +29,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	while (idx - 1)
 	{
-		current = current->next; /* Move forward 4 times if idx = 5 */
+		current = current->next;
 		idx--;
+		if (!current) /* If last node is NULL */
+		{
+			free(new);
+			return (NULL);
+		}
 	}
-	if (!current) /* If last node is NULL */
-	{
-		free(new);
-		return (NULL);
-	}
-	/* Insert the new node and update next and prev pointers */
-	next_node = current->next;
-	current->next = new;
-	next_node->prev = new;
+	/* Insert the new node */
+	next_node = current->next; /* Save the current node as the next node */
+	if (next_node)			   /* Case it was the last node */
+		next_node->prev = new;
+	current->next = new; /* update current node to have the new node */
+	/* Update next and prev pointers of the new node */
 	new->prev = current;
 	new->next = next_node;
 

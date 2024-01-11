332bfee46a0f957077b94697a1342b8c730224b5
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-11 21:07:28 +0200

    Insert new node at a given index of the dlistint.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0 || !*h)
 	{
 		new->next = *h;
-		(*h)->prev = new;
+		if (*h)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}

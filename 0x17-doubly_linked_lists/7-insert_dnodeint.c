91513e3c21a3024779e011fcae7ffa3030394706
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-11 21:24:44 +0200

    Insert new node at a given index of the dlistint.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *next_node, *new = malloc(sizeof(dlistint_t));
+	dlistint_t *current, *new = malloc(sizeof(dlistint_t));
 	/* Avoid derefrencing NULL pointers */
 	if (!h || !new)
 		return (NULL);
@@ -39,13 +39,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	}
 	/* Insert the new node */
-	next_node = current->next; /* Save the current node as the next node */
-	if (next_node)			   /* Case it was the last node */
-		next_node->prev = new;
+	if (current->next) /* Case it was the last node */
+		current->next->prev = new;
 	current->next = new; /* update current node to have the new node */
 	/* Update next and prev pointers of the new node */
 	new->prev = current;
-	new->next = next_node;
+	new->next = current->next;
 
 	return (new);
 }

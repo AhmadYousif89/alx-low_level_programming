b1966df1045df4b5bdf0a43e273e4b0cc5d45fb4
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-11 23:07:46 +0200

    Crackme 5 - Keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define ALPHABET "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk"
+#define KEY_LENGTH 6
+
 /**
  * main - generate a key depending on a username for crackme5
  * @argc: number of arguments passed
@@ -14,8 +17,7 @@ int main(int argc, char *argv[])
 {
 	unsigned int i, b;
 	size_t len, add;
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char p[7] = "      ";
+	char key[KEY_LENGTH + 1] = "      "; /* +1 for null char */
 
 	if (argc != 2)
 	{
@@ -23,24 +25,24 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	len = strlen(argv[1]);
-	p[0] = l[(len ^ 59) & 63];
+	key[0] = ALPHABET[(len ^ 59) & 63];
 	for (i = 0, add = 0; i < len; i++)
 		add += argv[1][i];
-	p[1] = l[(add ^ 79) & 63];
+	key[1] = ALPHABET[(add ^ 79) & 63];
 	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
-	p[2] = l[(b ^ 85) & 63];
+	key[2] = ALPHABET[(b ^ 85) & 63];
 	for (b = argv[1][0], i = 0; i < len; i++)
 		if ((char)b <= argv[1][i])
 			b = argv[1][i];
 	srand(b ^ 14);
-	p[3] = l[rand() & 63];
+	key[3] = ALPHABET[rand() & 63];
 	for (b = 0, i = 0; i < len; i++)
 		b += argv[1][i] * argv[1][i];
-	p[4] = l[(b ^ 239) & 63];
+	key[4] = ALPHABET[(b ^ 239) & 63];
 	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
 		b = rand();
-	p[5] = l[(b ^ 229) & 63];
-	printf("%s\n", p);
+	key[5] = ALPHABET[(b ^ 229) & 63];
+	printf("%s\n", key);
 	return (0);
 }

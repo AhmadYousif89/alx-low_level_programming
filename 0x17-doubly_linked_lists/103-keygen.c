6234a19cb2902cac5c8673dd20afeeee22407617
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-11 23:11:11 +0200

    Crackme 5 - Keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -7,6 +7,42 @@
 #define KEY_LENGTH 6
 
 /**
+ * generate_key - Generate a key based on the given username
+ * @username: input username
+ * @key: output key
+ */
+void generate_key(const char *username, char *key)
+{
+	size_t len, add;
+	unsigned int i, b;
+
+	len = strlen(username);
+	key[0] = ALPHABET[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += username[i];
+
+	key[1] = ALPHABET[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= username[i];
+
+	key[2] = ALPHABET[(b ^ 85) & 63];
+	for (b = username[0], i = 0; i < len; i++)
+		if ((char)b <= username[i])
+			b = username[i];
+
+	srand(b ^ 14);
+	key[3] = ALPHABET[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += username[i] * username[i];
+
+	key[4] = ALPHABET[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < username[0]; i++)
+		b = rand();
+
+	key[5] = ALPHABET[(b ^ 229) & 63];
+}
+
+/**
  * main - generate a key depending on a username for crackme5
  * @argc: number of arguments passed
  * @argv: arguments passed to main
@@ -15,8 +51,6 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i, b;
-	size_t len, add;
 	char key[KEY_LENGTH + 1] = "      "; /* +1 for null char */
 
 	if (argc != 2)
@@ -24,25 +58,8 @@ int main(int argc, char *argv[])
 		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-	len = strlen(argv[1]);
-	key[0] = ALPHABET[(len ^ 59) & 63];
-	for (i = 0, add = 0; i < len; i++)
-		add += argv[1][i];
-	key[1] = ALPHABET[(add ^ 79) & 63];
-	for (i = 0, b = 1; i < len; i++)
-		b *= argv[1][i];
-	key[2] = ALPHABET[(b ^ 85) & 63];
-	for (b = argv[1][0], i = 0; i < len; i++)
-		if ((char)b <= argv[1][i])
-			b = argv[1][i];
-	srand(b ^ 14);
-	key[3] = ALPHABET[rand() & 63];
-	for (b = 0, i = 0; i < len; i++)
-		b += argv[1][i] * argv[1][i];
-	key[4] = ALPHABET[(b ^ 239) & 63];
-	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
-		b = rand();
-	key[5] = ALPHABET[(b ^ 229) & 63];
+
+	generate_key(argv[1], key);
 	printf("%s\n", key);
 	return (0);
 }

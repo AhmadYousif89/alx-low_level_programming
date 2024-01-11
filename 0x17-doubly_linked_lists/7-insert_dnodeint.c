25d90c8518c627c5f333e9ce7968c21cd9a27b95
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-01-11 20:37:56 +0200

    Insert new node at a given index of the dlistint.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Insert new node at a given index of the dlistint.
+ * @h: pointer to the head of dlistint
+ * @idx: the position to insert the node
+ * @n: the data as a number
+ * Return: pointer to the new element or NULL on failuer
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current, *next_node, *new = malloc(sizeof(dlistint_t));
+	/* Avoid derefrencing NULL pointers */
+	if (!h || !new)
+		return (NULL);
+	/* Create new node */
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	/* Insert at the beginning of the list if idx = 0 */
+	if (!*h)
+		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	/* Traverse the list to idx position */
+	current = *h;
+	while (idx - 1)
+	{
+		current = current->next; /* Move forward 4 times if idx = 5 */
+		idx--;
+	}
+	if (!current) /* If last node is NULL */
+	{
+		free(new);
+		return (NULL);
+	}
+	/* Insert the new node and update next and prev pointers */
+	next_node = current->next;
+	current->next = new;
+	next_node->prev = new;
+	new->prev = current;
+	new->next = next_node;
+
+	return (new);
+}

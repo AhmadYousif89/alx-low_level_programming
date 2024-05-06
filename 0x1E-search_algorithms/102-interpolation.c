37003099ca140d845b0d7a29c0c3ded679774588
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-05-06 20:16:10 +0300

    interpolation_search - searches for a value in an array of integers

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,18 +11,18 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-		return (-1);
-
 	int low = 0;
 	int high = size - 1;
+
+	if (array == NULL || size == 0)
+		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		size_t pos = low + (((double)(high - low) / (array[high] - array[low])) *
 							(value - array[low]));
 
-		printf("Value checked array[%zu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
 			return (pos);
@@ -32,6 +32,6 @@ int interpolation_search(int *array, size_t size, int value)
 			high = pos - 1;
 	}
 
-	printf("Value checked array[%zu] is out of range\n", high);
+	printf("Value checked array[%u] is out of range\n", high);
 	return (-1);
 }
